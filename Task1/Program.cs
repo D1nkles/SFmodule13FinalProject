1aeaecf5b37d5c20926bb8d2843b1641601bfe5a
Ilia <isinetov@mail.ru>
2024-05-14 00:45:32 +0400

    Исправил ошибку в 1 задании, из-за которой запись символов из текстового файла происходила некореектно.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,45 +2,44 @@
 
 class Program 
 {
-    static void Main(string[] args) 
+    static void Main(string[] args)
     {
         Console.WriteLine("Введите путь до текста: ");
         string PATH = Console.ReadLine();
-        if (!string.IsNullOrEmpty(PATH) && File.Exists(PATH)) 
+        if (!string.IsNullOrEmpty(PATH) && File.Exists(PATH))
         {
-            using FileStream fileStream = new FileStream(PATH, FileMode.Open);
+            List<char> characterArr = new List<char>();
+
+            StreamReader sr = new StreamReader(PATH);
+
+            do
             {
-                char[] characterArr = new char[fileStream.Length];
+                characterArr.Add((char)sr.Read());
 
-                while (fileStream.Position < fileStream.Length)
-                {
-                    StreamReader sr = new StreamReader(fileStream);
-
-                    characterArr[fileStream.Position] = (char)sr.Read();
-                }
-
-                List<char> listTest = new List<char>();
-                LinkedList<char> linkedListTest = new LinkedList<char>();
-
-                Stopwatch sw = new Stopwatch();
-
-                sw.Start();
-                for (int i = 0; i < characterArr.Length; i++)
-                {
-                    listTest.Add(characterArr[i]);
-                }
-                sw.Stop();
-                Console.WriteLine("Время добавления в эл-ов в List: " + sw.Elapsed.TotalMilliseconds);
-
-                sw.Restart();
-                for (int i = 0; i < characterArr.Length; i++)
-                {
-                    linkedListTest.AddLast(characterArr[i]);
-                }
-                sw.Stop();
-
-                Console.WriteLine("Время добавления в эл-ов в LinkedList: " + sw.Elapsed.TotalMilliseconds);
             }
+            while (!sr.EndOfStream);
+
+            List<char> listTest = new List<char>();
+            LinkedList<char> linkedListTest = new LinkedList<char>();
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            for (int i = 0; i < characterArr.Count; i++)
+            {
+                listTest.Add(characterArr[i]);
+            }
+            sw.Stop();
+            Console.WriteLine("Время добавления в эл-ов в List: " + sw.Elapsed.TotalMilliseconds);
+
+            sw.Restart();
+            for (int i = 0; i < characterArr.Count; i++)
+            {
+                linkedListTest.AddLast(characterArr[i]);
+            }
+            sw.Stop();
+
+            Console.WriteLine("Время добавления в эл-ов в LinkedList: " + sw.Elapsed.TotalMilliseconds);
         }
     }
 }
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,15 +1,53 @@
-﻿class Program
+﻿using static System.Net.Mime.MediaTypeNames;
+
+class Program
 {
     static void Main(string[] args)
     {
         using FileStream fileStream = new FileStream("C:\\Users\\Dinkles\\Downloads\\Text1.txt", FileMode.Open);
         {
-            char[] characterArr = new char[fileStream.Length];
+            List<char> characters = new List<char>();
+            List<string> words = new List<string>();
+            HashSet<string> uniqueWords = new HashSet<string>();
+            Dictionary<string, int> wordsStat = new Dictionary<string, int>();
 
             while (fileStream.Position < fileStream.Length)
             {
                 StreamReader sr = new StreamReader(fileStream);
+                char character = (char)sr.Read();
+                
+                if (!char.IsPunctuation(character) && !char.IsNumber(character) &&
+                    character != 'I' && character != 'V' && character != 'X') 
+                {
+                    characters.Add(character);
+                }
+
+                else 
+                {
+                    if (characters.Count > 0) 
+                    {
+                        string word = characters.ToString();
+                        words.Add(word);
+                        characters.Clear();
+                    }
+                }
             }
+
+            uniqueWords = new HashSet<string>(words);
+
+            foreach(string uniqueWord in uniqueWords) 
+            {
+                int wordCount = 0;
+                foreach(string wrd in words) 
+                {
+                    if (uniqueWord == wrd) 
+                    {
+                        wordCount++;
+                    }
+                }
+                wordsStat.Add(uniqueWord, wordCount);
+            }
+
         }
     }
 }
