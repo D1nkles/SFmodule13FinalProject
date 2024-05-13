192211c9957bd1ae6b2b52298440efd31ed405aa
Ilia <isinetov@mail.ru>
2024-05-13 23:25:16 +0400

    Задание 13.6.1. Наша задача — сравнить производительность вставки в List<T> и LinkedList<T>.Для этого используйте уже знакомый вам StopWatch.
    На примере этого текста, выясните, какие будут различия между этими коллекциями.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,41 @@
-﻿class Program 
+﻿using System.Diagnostics;
+
+class Program 
 {
     static void Main(string[] args) 
     {
-        
+        using FileStream fileStream = new FileStream("C:\\Users\\Dinkles\\Downloads\\Text1.txt", FileMode.Open);
+        {
+            char[] characterArr = new char[fileStream.Length];
+
+            while (fileStream.Position < fileStream.Length)
+            {
+                StreamReader sr = new StreamReader(fileStream);
+
+                characterArr[fileStream.Position] = (char)sr.Read();
+            }
+
+            List<char> listTest = new List<char>();
+            LinkedList<char> linkedListTest = new LinkedList<char>();
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            for (int i = 0; i < characterArr.Length ; i++) 
+            {
+                listTest.Add(characterArr[i]);
+            }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+
+            sw.Restart();
+            for (int i = 0; i < characterArr.Length; i++)
+            {
+                linkedListTest.AddLast(characterArr[i]);
+            }
+            sw.Stop();
+
+            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+        }
     }
 }
