60b8d87412c4ca8e5190d022e0d0eb5dd713dd7e
Ilia <isinetov@mail.ru>
2024-05-13 23:37:19 +0400

    Доработал Задание 1.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,38 +4,43 @@ class Program
 {
     static void Main(string[] args) 
     {
-        using FileStream fileStream = new FileStream("C:\\Users\\Dinkles\\Downloads\\Text1.txt", FileMode.Open);
+        Console.WriteLine("Введите путь до текста: ");
+        string PATH = Console.ReadLine();
+        if (!string.IsNullOrEmpty(PATH) && File.Exists(PATH)) 
         {
-            char[] characterArr = new char[fileStream.Length];
-
-            while (fileStream.Position < fileStream.Length)
+            using FileStream fileStream = new FileStream(PATH, FileMode.Open);
             {
-                StreamReader sr = new StreamReader(fileStream);
+                char[] characterArr = new char[fileStream.Length];
 
-                characterArr[fileStream.Position] = (char)sr.Read();
+                while (fileStream.Position < fileStream.Length)
+                {
+                    StreamReader sr = new StreamReader(fileStream);
+
+                    characterArr[fileStream.Position] = (char)sr.Read();
+                }
+
+                List<char> listTest = new List<char>();
+                LinkedList<char> linkedListTest = new LinkedList<char>();
+
+                Stopwatch sw = new Stopwatch();
+
+                sw.Start();
+                for (int i = 0; i < characterArr.Length; i++)
+                {
+                    listTest.Add(characterArr[i]);
+                }
+                sw.Stop();
+                Console.WriteLine("Время добавления в эл-ов в List: " + sw.Elapsed.TotalMilliseconds);
+
+                sw.Restart();
+                for (int i = 0; i < characterArr.Length; i++)
+                {
+                    linkedListTest.AddLast(characterArr[i]);
+                }
+                sw.Stop();
+
+                Console.WriteLine("Время добавления в эл-ов в LinkedList: " + sw.Elapsed.TotalMilliseconds);
             }
-
-            List<char> listTest = new List<char>();
-            LinkedList<char> linkedListTest = new LinkedList<char>();
-
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
-            for (int i = 0; i < characterArr.Length ; i++) 
-            {
-                listTest.Add(characterArr[i]);
-            }
-            sw.Stop();
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
-
-            sw.Restart();
-            for (int i = 0; i < characterArr.Length; i++)
-            {
-                linkedListTest.AddLast(characterArr[i]);
-            }
-            sw.Stop();
-
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
         }
     }
 }
---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        using FileStream fileStream = new FileStream("C:\\Users\\Dinkles\\Downloads\\Text1.txt", FileMode.Open);
+        {
+            char[] characterArr = new char[fileStream.Length];
+
+            while (fileStream.Position < fileStream.Length)
+            {
+                StreamReader sr = new StreamReader(fileStream);
+            }
+        }
+    }
+}
